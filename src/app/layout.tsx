a8b2327b299596c7eb769ba5eb16780bc5691afb
Fauzan Azhima <bintangtinggi2014@gmail.com>
2026-08-06 17:39:58 +0700

    udpate pada backend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Geist } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/navbar";
-import { Footer } from "@/components/footer";
 import { cn } from "@/lib/utils";
 import { NextAuthProvider } from "@/components/session-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -26,11 +24,7 @@ export default function RootLayout({
       <body className={`${inter.className} min-h-full flex flex-col`}>
         <NextAuthProvider>
           <TooltipProvider>
-            <Navbar />
-            <main className="flex-1">
-              {children}
-            </main>
-            <Footer />
+            {children}
           </TooltipProvider>
         </NextAuthProvider>
       </body>
